495fe41ffd7d89f8ae01cecc711db2000d2e0a5f
Luke Winslow <lawinslow@gmail.com>
2017-05-22 10:40:44 -0400

    Fix ice-year to be consistent north/south
     - How ice-on/ice-off is organized has no standard.
     - Here I try to standardize it on summer-centered
       ice datasets. So ice-on and ice-off are on the
       row which corresponds with the summer season

--- R/lter_ice_onoff.R
+++ R/lter_ice_onoff.R
@@ -8,6 +8,11 @@
 #' Southern data are reported only as "ice_on" and "ice_off". To match the northern data, I have renamed them 
 #' "datelastopen" and "datefirstopen" respectively. I feel this is the closest analog. 
 #' 
+#' Further, there is a "year" column. There is no clear consensus on what season or year should correspond with 
+#' in ice dates. For example, sometimes ice-on can be in December, and sometimes in January. What "year" is that 
+#' ice date associated with? Here, I have normalized everything so that "year" corresponds with the *summer* after
+#' an ice-out date and before the ice-on date. So ice-on in January 1984 corresponds to the "year" 1983. 
+#' 
 #' 
 #' @import httr
 #' @import dplyr
@@ -23,6 +28,10 @@ lter_ice_onoff = function(){
   r = GET('https://lter.limnology.wisc.edu/file/11471/download?token=nXC_mWDNhqvwGL8JiqS2Zu9Dg1qGK6Jq0FzmBYFT1E8')
   south = content(r) %>% transmute(lakeid, year=year4, datelastopen=mdy(ice_on), datefirstopen=mdy(ice_off), 
                                    datelastice=NA, datefirstice=NA)
+  
+  #ok, now we must lag
+  south = group_by(south, lakeid) %>% mutate(datefirstopen=lag(datefirstopen)) %>% tail
+  
   #d$date = as.POSIXct(d$sampledate)
   
   d = rbind(north, south)
